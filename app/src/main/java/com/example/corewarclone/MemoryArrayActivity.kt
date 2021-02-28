f40d1e2dd8abebecc80e04c9954f2d7888e6d567
Liaihua <ilyavovchenko431@gmail.com>
2021-02-28 17:53:17 +1000

    Repacking my repo in another place

---
+++ app/src/main/java/com/example/corewarclone/MemoryArrayActivity.kt
@@ -0,0 +1,12 @@
+package com.example.corewarclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// Я не знаю, как мне сделать эту активность: на Kotlin, или на C++?
+class MemoryArrayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_memory_array)
+    }
+}
