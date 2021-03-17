b658652dc54ac8be9543951c68ab52a846812c33
Ваше Имя <you@example.com>
2021-03-17 13:24:36 +1000

    Managed some things with programFileAdapter and MainActivity Button works fine, but programs_recycler_view is broken. Message of error:
    E/RecyclerView: No layout manager attached; skipping layout

--- app/src/main/java/com/example/corewarclone/mainActivity/MainActivity.kt
+++ app/src/main/java/com/example/corewarclone/mainActivity/MainActivity.kt
@@ -2,59 +2,27 @@ package com.example.corewarclone.mainActivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import androidx.recyclerview.widget.RecyclerView
 import com.example.corewarclone.R
-import java.util.*
-
-class ProgramFileAdapter(private val programFiles: Array<ProgramFile>) : RecyclerView.Adapter<ProgramFileAdapter.ViewHolder>() {
-
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val fileNameTextView: TextView
-        val fileLastEditTextView: TextView
-        val fileSizeTextView: TextView
-
-        init {
-            fileNameTextView = view.findViewById(R.id.file_name)
-            fileLastEditTextView = view.findViewById(R.id.file_date_time)
-            fileSizeTextView = view.findViewById(R.id.file_size)
-        }
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.program_file_item, parent, false)
-
-        return ViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.fileNameTextView.text = programFiles[position].name
-        holder.fileSizeTextView.text = programFiles[position].size.toString()
-        holder.fileLastEditTextView.text = programFiles[position].last_edit.toString()
-    }
-
-    override fun getItemCount(): Int {
-        return programFiles.size
-    }
-}
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 class MainActivity : AppCompatActivity() {
-    val programFileManager: ProgramFileManager = ProgramFileManager()
+    private val programFileManager: ProgramFileManager = ProgramFileManager()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val programFileList = programFileManager.listProgramFiles()
         val programFileAdapter = ProgramFileAdapter(programFileList)
-        var pf_recyclerView = findViewById<RecyclerView>(R.id.programs_recycler_view)
-        pf_recyclerView.adapter = programFileAdapter
+        var pfRecyclerView = findViewById<RecyclerView>(R.id.programs_recycler_view)
+        pfRecyclerView.adapter = programFileAdapter
     }
 
     fun newFile(view: View) {
-
+        var button = view.findViewById<Button>(R.id.fab)
+        button.text = Random.nextInt(100).toString()
     }
 }
--- app/src/main/java/com/example/corewarclone/mainActivity/ProgramFileManager.kt
+++ app/src/main/java/com/example/corewarclone/mainActivity/ProgramFileManager.kt
@@ -1,20 +1,45 @@
 package com.example.corewarclone.mainActivity
 
+import java.io.*
 import java.util.*
 import kotlin.*
+
+const val dir_name = "redcode";
+
 // ProgramFile используется для представления основных файлов, хранимых в папке приложения
-data class ProgramFile (val name: String, val last_edit: Long, val size: Int);
+data class ProgramFile (val name: String, val last_edit: Long, val size: Long);
 
 class ProgramFileManager {
 
     // Метод создает директорию "redcode/" (мспользуется в случае, если директория отсутствует)
-    fun initializeRedcodeDirectory() {
-
+    private fun initializeRedcodeDirectory() {
+        File(dir_name).mkdir()
     }
 
     // Метод используется для отображения файлов, которые находятся в поддиректории приложения "redcode/"
-    fun listProgramFiles() : Array<ProgramFile> {
-        return arrayOf(ProgramFile("test", Date().time, 1))
+    fun listProgramFiles() : Array<ProgramFile>? {
+
+        val redcodeDir = File(dir_name)
+
+        if(!redcodeDir.exists()) {
+            initializeRedcodeDirectory()
+            return null
+        }
+
+        var programFiles = arrayOf<ProgramFile>()
+
+        for (redcodeFile in redcodeDir.listFiles())
+        {
+            val pf = ProgramFile(
+                redcodeFile.name,
+                redcodeFile.lastModified(),
+                redcodeFile.totalSpace
+            )
+
+            programFiles += pf
+        }
+
+        return programFiles
     }
 
     // Метод используется для сохранения программы из редактора в папку "redcode/"
