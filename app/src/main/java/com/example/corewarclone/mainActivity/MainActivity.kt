8535c2c69f233a47f295b0ff2bbf8414eda6100c
Liaihua <ilyavovchenko431@gmail.com>
2021-03-18 21:09:01 +1000

    update

--- app/src/main/java/com/example/corewarclone/mainActivity/MainActivity.kt
+++ app/src/main/java/com/example/corewarclone/mainActivity/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.corewarclone.mainActivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.corewarclone.R
+import com.example.corewarclone.editorActivity.EditorActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class ProgramFileAdapter(private val programFiles: Array<ProgramFile>) : RecyclerView.Adapter<ProgramFileAdapter.ViewHolder>() {
@@ -21,6 +25,12 @@ class ProgramFileAdapter(private val programFiles: Array<ProgramFile>) : Recycle
             fileNameTextView = view.findViewById(R.id.file_name)
             fileLastEditTextView = view.findViewById(R.id.file_date_time)
             fileSizeTextView = view.findViewById(R.id.file_size)
+        }
+
+        fun bind(pf: ProgramFile) {
+            fileNameTextView.text = pf.name
+            fileLastEditTextView.text = pf.last_edit.toString()
+            fileSizeTextView.text = pf.size.toString()
         }
     }
 
@@ -52,9 +62,17 @@ class MainActivity : AppCompatActivity() {
         val programFileAdapter = ProgramFileAdapter(programFileList)
         var pf_recyclerView = findViewById<RecyclerView>(R.id.programs_recycler_view)
         pf_recyclerView.adapter = programFileAdapter
+
+        val button = findViewById<Button>(R.id.fab)
+        button.setOnClickListener {
+            newFile(it)
+        }
     }
 
-    fun newFile(view: View) {
+    // Задолбал уже отвлекаться
 
+    fun newFile(view: View) {
+        val intent = Intent(this, EditorActivity::class.java)
+        startActivity(intent)
     }
 }
