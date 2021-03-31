f98b76ca339f4278691bf5482110ffaa91e4112e
Ваше Имя <you@example.com>
2021-03-31 13:42:46 +1000

    Задания внутри. Сделай их всех!

--- app/src/main/java/com/example/corewarclone/editorActivity/AssembledProgramDialogFragment.kt
+++ app/src/main/java/com/example/corewarclone/editorActivity/AssembledProgramDialogFragment.kt
@@ -1,7 +1,17 @@
 package com.example.corewarclone.editorActivity
 
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 
+// TODO Сделать отображение "скомпилированного" кода
 class AssembledProgramDialogFragment : DialogFragment(){
-
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity.let {
+            val builder = AlertDialog.Builder(it)
+            builder.setMessage("Assembled view")
+            builder.create()
+        }
+    }
 }
