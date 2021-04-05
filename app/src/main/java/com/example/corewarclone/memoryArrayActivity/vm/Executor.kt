65bda0eec20bb107cff88778af8a0e6e778f945c
Ваше Имя <you@example.com>
2021-04-05 12:25:36 +1000

    Подумай, в какой класс класть MemoryArray и Warrior'ов

--- app/src/main/java/com/example/corewarclone/memoryArrayActivity/vm/Executor.kt
+++ app/src/main/java/com/example/corewarclone/memoryArrayActivity/vm/Executor.kt
@@ -1,7 +1,46 @@
 package com.example.corewarclone.memoryArrayActivity.vm
 
 class Executor {
-    fun execute() {
+    // Выполнение инструкции. Метод возвращает число, на которое увеличить Instruction Pointer
+    // Если возвращается null, то выполняемый Task завершается
 
+    // #, $, @, <
+
+    fun execute(instruction: Instruction) : Int? {
+        when(instruction.opcode) {
+            // DAT
+            0.toByte() -> {
+                return null
+            }
+
+            // MOV
+            1.toByte() -> {
+
+            }
+
+            // ADD
+            2.toByte() -> {}
+
+            // SUB
+            3.toByte() -> {}
+
+            // JMP
+            4.toByte() -> {}
+
+            // JMZ
+            5.toByte() -> {}
+
+            // JMN
+            6.toByte() -> {}
+
+            // DJN
+            7.toByte() -> {}
+
+            // CMP
+            8.toByte() -> {}
+
+            // SPL
+            9.toByte() -> {}
+        }
     }
 }
