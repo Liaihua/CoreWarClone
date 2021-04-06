bf92ec48e6bbb48d887352c8fad1dfa7e4790681
Ваше Имя <you@example.com>
2021-04-06 11:13:53 +1000

    Все еще делаю ВМ

--- app/src/main/java/com/example/corewarclone/memoryArrayActivity/vm/Executor.kt
+++ app/src/main/java/com/example/corewarclone/memoryArrayActivity/vm/Executor.kt
@@ -1,7 +1,8 @@
 package com.example.corewarclone.memoryArrayActivity.vm
 
 class Executor {
-    // Выполнение инструкции. Метод execute возвращает число, на которое увеличить Instruction Pointer
+    // Метод execute выполняет текущую инструкцию и ВОЗВРАЩАЕТ ЧИСЛО (А НЕ ВРУЧНУЮ МАНИПУЛИРУЕТ ВСЕМ И СРАЗУ),
+    // на которое увеличить Instruction Pointer
     // Если возвращается null, то выполняемый Task завершается
 
     // # - 0,
@@ -17,6 +18,7 @@ class Executor {
         return (operand shr 16) and 0xFF
     }
 
+    // Вычисляет значение относительного адреса
     private fun calculateAddress(instruction: Instruction) : Pair<Int, Int>? {
 
         var operandAAddress = 0
@@ -24,7 +26,9 @@ class Executor {
         when(getOperandAddressMode(instruction.operandA)) {
 
             // #
-            0 -> {}
+            0 -> {
+
+            }
             // $
             1 -> {}
             // @
@@ -37,12 +41,32 @@ class Executor {
 
         when(getOperandAddressMode(instruction.operandB)) {
 
+            // #
+            0 -> {}
+            // $
+            1 -> {}
+            // @
+            2 -> {}
+            // <
+            3 -> {}
+            else -> return null
         }
-        return null
+
+        return Pair(operandAAddress, operandBAddress)
     }
 
-    fun execute(instruction: Instruction) : Int? {
-        val operandsAddresses = calculateAddress(instruction)
+    fun execute(warrior: Warrior, task: Task, instruction: Instruction) : Int? {
+        // Мы предварительно вычисляем адреса, на которые ссылаются операнды, дабы ускорить
+        val operandsAddresses = calculateAddress(instruction) ?: return null
+
+
+        // Мы дополнительно берем режимы адресации, дабы не спутать $0 с #0
+        // (ведь calculateAddress вычисляет именно значение адреса, но не уточняет, какой режим
+        // адресации для данного операнда)
+        val operandsModes = Pair(
+            getOperandAddressMode(instruction.operandA),
+            getOperandAddressMode(instruction.operandB))
+
         when(instruction.opcode) {
             // DAT
             0.toByte() -> {
@@ -51,32 +75,76 @@ class Executor {
 
             // MOV
             1.toByte() -> {
-
+                if(operandsModes.second == 0) {
+                    return null
+                }
+                else {
+                    
+                }
             }
 
             // ADD
-            2.toByte() -> {}
+            2.toByte() -> {
+                if(operandsModes.second != 0) {
+
+                }
+                else {
+                    return null
+                }
+            }
 
             // SUB
-            3.toByte() -> {}
+            3.toByte() -> {
+                if(operandsModes.second != 0) {
+
+                }
+                else {
+                    return null
+                }
+            }
 
             // JMP
-            4.toByte() -> {}
+            4.toByte() -> {
+                return calculateRound(MEMORY_ARRAY_SIZE, task.instructionPointer + operandsAddresses.first)
+            }
 
             // JMZ
-            5.toByte() -> {}
+            5.toByte() -> {
+                return if(operandsAddresses.second == 0) {
+                    calculateRound(MEMORY_ARRAY_SIZE, task.instructionPointer + operandsAddresses.first)
+                } else {
+                    ++task.instructionPointer
+                }
+            }
 
             // JMN
-            6.toByte() -> {}
+            6.toByte() -> {
+                if(operandsAddresses.second != 0) {
+                    task.instructionPointer = calculateRound(MEMORY_ARRAY_SIZE, task.instructionPointer + operandsAddresses.first)
+                }
+                else {
+                    task.instructionPointer++
+                }
+            }
 
             // DJN
             7.toByte() -> {}
 
             // CMP
-            8.toByte() -> {}
+            8.toByte() -> {
+
+            }
 
             // SPL
-            9.toByte() -> {}
+            9.toByte() -> {
+                if(warrior.taskQueue.count() <= MAX_TASKS)
+                {
+                    val newTask = Task()
+                    newTask.instructionPointer = operandsAddresses.first
+                    warrior.taskQueue.add(newTask)
+                }
+                return ++task.instructionPointer
+            }
         }
         return null
     }
--- app/src/main/java/com/example/corewarclone/memoryArrayActivity/vm/Scheduler.kt
+++ app/src/main/java/com/example/corewarclone/memoryArrayActivity/vm/Scheduler.kt
@@ -4,6 +4,11 @@ import java.util.*
 
 // TODO Догадайся
 
+const val CYCLES_UNTIL_TIE = 100000
+const val MAX_TASKS = 64
+
+var Warriors = arrayOf<Warrior>()
+
 class Warrior {
     var id: Int = 0
     var name: String = ""
@@ -12,7 +17,7 @@ class Warrior {
 }
 
 class Task {
-    var instructionPointer: Int = -1
+    var instructionPointer: Int = 0
 }
 
 class Scheduler {
